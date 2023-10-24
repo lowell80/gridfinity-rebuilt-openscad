74c288377729ec20f32e95b25835d7f3f94b7ad7
Lowell Alleman <lowell.alleman@cdillc.com>
2023-10-23 23:55:49 -0400

    Add jinja2 support
    - Move path generation to jinja2 template; drop as first-class output
    - Complete support for nozzle_diameter and filament_type

--- build_all.py
+++ build_all.py
@@ -7,7 +7,19 @@ from pathlib import Path, PurePosixPath
 from subprocess import call
 from typing import Any, Callable, Dict, Iterator, List
 
-from jinja2 import environment
+from jinja2 import Environment, StrictUndefined, FileSystemLoader
+
+jinja_env = Environment(autoescape=True,
+                        undefined=StrictUndefined,
+                        loader=FileSystemLoader("."),
+                        auto_reload=False)
+
+
+def jinja_render(template, **args):
+    if "{{" in template or "{%}" in template:
+        return jinja_env.from_string(template).render(**args)
+    else:
+        return template
 
 
 OPENSCAD_BIN = "/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
@@ -17,24 +29,6 @@ SLICER_BIN = "/Applications/PrusaSlicer.app/Contents/MacOS/PrusaSlicer"
 
 def path_append_dash_suffix(path: PurePosixPath, value) -> PurePosixPath:
     return path.with_name(path.name + f"{value}-")
-
-
-def mapping_sub_path(mapping: Dict[Any, str]):
-    def to_path(path: PurePosixPath, value) -> PurePosixPath:
-        name = mapping.get(value, str(value))
-        if name:
-            return path.with_name(path.name + f"-{name}")
-        return path
-    return to_path
-
-
-def mapping_sub_dirandpath(mapping: Dict[Any, str]):
-    def to_path(path: PurePosixPath, value) -> PurePosixPath:
-        name = mapping.get(value, str(value))
-        if name:
-            return path.parent / name / (path.name + f"-{name}")
-        return path
-    return to_path
 
 
 def openscad_arg(var_name):
@@ -53,14 +47,12 @@ def expand_xy(min, max, block=()):
 class Factor:
     name: str = field()
     values: tuple | CmdGenerator = field()
-    to_command: Callable[[Any], List[str]] = field()
+    to_command: Callable[[Any], List[str]] = lambda _: []
     to_meta: Callable[[Any], str] = lambda v: str(v)
-    to_path: Callable[[PurePosixPath, Any], PurePosixPath] = lambda p, _: p
-    rewrite_args: Callable[[List[str], Any], List[str]] = lambda args, _: args
 
     def __iter__(self):
         if isinstance(self.values, CmdGenerator):
-            return (res.path for res in self.values.build_commands())
+            return self.values.build_commands()
         else:
             return iter(self.values)
 
@@ -76,48 +68,32 @@ class CmdGeneratorResult:
 class CmdGenerator:
     cmd: list[str] = field()            # default_factory=list)
     factors: list[Factor] = field()     # default_factory=list)
-    path_suffix: str = field()
-    init_path: PurePosixPath = PurePosixPath(".")
+    vars: dict[str, str]
+    path: str
 
     def product(self):
         return itertools.product(*(factor for factor in self.factors))
 
     def build_commands(self) -> Iterator[CmdGeneratorResult]:
         for combos in self.product():
-            path = self.init_path
             cmd_args = self.cmd.copy()
             meta = {}
             for i, value in enumerate(combos):
                 factor = self.factors[i]
-                meta[factor.name] = factor.to_meta(value)
-                cmd_args.extend(factor.to_command(value))
-                path = factor.to_path(path, value)
-                cmd_args = factor.rewrite_args(cmd_args, value)
-            path = path.with_suffix(self.path_suffix)
-            path, cmd_args, meta = self.finalize_command(path, cmd_args, meta)
+                if isinstance(value, CmdGeneratorResult):
+                    meta.update(value.meta)
+                else:
+                    meta[factor.name] = factor.to_meta(value)
+                    cmd_args.extend(factor.to_command(value))
+
+            meta.update({key: jinja_render(value, **meta)
+                         for key, value in self.vars.items()})
+
+            cmd_args = [jinja_render(arg, **meta) for arg in cmd_args]
+            path = Path(jinja_render(self.path, **meta))
+
+            # path, cmd_args, meta = self.finalize_command(path, cmd_args, meta)
             yield CmdGeneratorResult(path, cmd_args, meta)
-
-    @staticmethod
-    def finalize_command(path, cmd_args: list[str], meta):
-        idx = cmd_args.index("OUTPUT_PATH")
-        cmd_args[idx] = os.fspath(path)
-        return path, cmd_args, meta
-
-
-def lite_bin_rewrite(args, value):
-    '''
-    lambda args, value: args if value != "lite" else
-                    [v if a == "gridfinity-rebuilt-bins.scad" else a
-                     for a in args]
-    '''
-    if value == "lite":
-        new_args = []
-        for arg in args:
-            if arg == "gridfinity-rebuilt-bins.scad":
-                arg = "gridfinity-rebuilt-lite.scad"
-            new_args.append(arg)
-        return new_args
-    return args
 
 
 scad_gen = CmdGenerator(
@@ -125,41 +101,35 @@ scad_gen = CmdGenerator(
         OPENSCAD_BIN,
         "--export-format=binstl",
         "--enable", "fast-csg",
-        "-o", "OUTPUT_PATH",
-        "gridfinity-rebuilt-bins.scad",
+        "-o", "{{ stl_path }}",
+        "gridfinity-rebuilt-{{ 'lite' if base == 'lite' else 'bins' }}.scad",
     ],
     [
         Factor("base_size",
                expand_xy(1, 5, ((5, 5),)),
-               to_path=lambda path, value: path.with_name(
-                   path.name + f"-{value[0]}x{value[1]}"),
                to_command=lambda value: ["-D", f"gridx={value[0]}",
                                          "-D", f"gridy={value[1]}"],
                to_meta=lambda value: f"{value[0]}x{value[1]}",
                ),
         Factor("height",
                (2, 4, 6, 8, 10, 12),
-               to_path=lambda path, value: path.with_name(
-                   path.name + f"-{value}h"),
                to_command=openscad_arg("gridz"),
                ),
         Factor("base",
                (0, 1, "lite"),
-               to_path=mapping_sub_dirandpath({0: "flat", 1: "magnet"}),
                to_command=lambda value: [
-                   # Not sure why this defaults incorrectly....
+                   # Not sure why these defaults incorrectly....
                    "-D", f"style_hole={value}"] if value != "lite" else ["-D", "divx=1",
                                                                          "-D", "divy=1"],
                to_meta=lambda v: {0: "flat",
-                                  1: "magnet-hole", "lite": "lite"}[v],
-               rewrite_args=lite_bin_rewrite,
+                                  1: "magnet",
+                                  "lite": "lite"}[v]
                ),
         Factor("lip",
                (0, 2),
-               to_path=mapping_sub_dirandpath({0: "stackable", 2: "nolip"}),
                to_command=openscad_arg("style_lip"),
                to_meta=lambda v: {
-                   0: "lip", 2: "disable lib, retain height"}[v],
+                   0: "stackable", 2: "open"}[v],
                ),
         Factor("tab",
                (5,),
@@ -171,15 +141,11 @@ scad_gen = CmdGenerator(
                to_command=openscad_arg("scoop"),
                ),
     ],
-    init_path=Path("output/bin"),
-    path_suffix=".stl"
+    vars={
+        "stl_path": "output/gridfinity/models/bins/{{ base }}-{{ lip }}/bin-{{ base_size }}-{{ height }}h-{{ base }}-{{ lip }}.stl",
+    },
+    path="{{ stl_path }}"
 )
-
-# Jinja2
-stl_template = "output/models/{{ base }}-{{ lip }}/bin-{{ base_size }}-{{ height }}h-{{ base }}-{{ lip }}.stl"
-gcode_dir_template = "output/gcode/{{ filament_type }}-n{{ nozzle_diameter}}/{{ base }}-{{ lip }}"
-# Actual file name isn't known until after render....  (no easy way to know without parsing stdout or building into a fresh temp directory.... )
-
 
 """
 
@@ -204,24 +170,20 @@ slicer_gen = CmdGenerator(
         SLICER_BIN,
         "--export-gcode",
         # "--export-3mf",
-        "--output", "OUTPUT_PATH",
+        "--load", "profile_{{ filament_type }}_n{{ nozzle_diameter }}.ini",
+        "--output", "{{ gcode_path }}",
+        "{{ stl_path }}"
     ],
     [
-        Factor("model",
-               scad_gen,
-               to_command=lambda value: [os.fspath(value)],
-               ),
-        Factor("slicer_config",
-               ("pla",
-                "petg",
-                ),
-               to_command=lambda value: ["--load", f"profile_{value}_n06.ini"],
-               to_path=lambda path, value: path / str(value).upper(),
-               ),
-
+        Factor("model", scad_gen),
+        Factor("filament_type", ("pla", "petg")),
+        Factor("nozzle_diameter", ("06",)),
     ],
-    init_path=Path("output"),
-    path_suffix=""
+    vars={
+        # Add a "/models" layer in there...
+        "gcode_path": "output/gridfinity/gcode/bins/{{ filament_type }}-n{{ nozzle_diameter}}/{{ base }}-{{ lip }}"
+    },
+    path="{{ gcode_path }}"
 )
 
 
